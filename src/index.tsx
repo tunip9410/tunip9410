195695109d5b468c884c449f1f4e5b59d1d26773
tunip9410 <tunip943@gmail.com>
2023-01-02 03:08:47 +0900

    🏗️setting up

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style/style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+
+function reducer(currentState :any, action :any) {
+    if (currentState === undefined) {
+        return {
+            page: "home"
+        }
+    }
+
+    const newState = {...currentState}
+
+    return newState
+}
+
+const store = createStore(reducer)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
